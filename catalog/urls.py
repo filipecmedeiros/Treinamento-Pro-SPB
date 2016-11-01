0984308a2dd698ddae540bb3721af2637775e78b
filipecmedeiros <fcma@cin.ufpe.br>
2016-11-01 11:50:31 -0300

    Finalizando módulo 3

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.product_list, name='product_list'),
-	url(r'^(?P<slug>[\w_-]+)/$', views.category, name='category'),
+	url(r'^$', views.ProductListView.as_view(), name='product_list'),
+	url(r'^(?P<slug>[\w_-]+)/$', views.CategoryView.as_view(), name='category'),
 	url(r'^produtos/(?P<slug>[\w_-]+)/$', views.product, name='product'),
 ]
